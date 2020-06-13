c4299588564a3d69a3fb877367afad80bb6c72df
Rohan Hirekerur <rohanhirekerur@gmail.com>
2020-06-13 12:57:44 +0000

    Comments and refactoring

--- database/db_handler.py
+++ database/db_handler.py
@@ -1,3 +1,28 @@
+# DatabaseHandler class :
+# For interfacing python with mysql database. 
+# 
+# Tables :
+#   -   Customer
+#   -   Account
+#   -   userstore
+#   -   CustomerStatus
+#   -   AccountStatus
+#   -   Transactions
+#
+# Functions :
+#   -   __init__(self)
+#   -   add_customer_status(self, SSN_ID, Customer_ID, Status, Message, Last_Updated)
+#   -   get_customer_status_from_Customer_ID(self, Customer_ID)
+#   -   get_all_customer_status(self)
+#   -   add_account_status(self, Customer_ID, Account_ID, Account_Type, Status, Message, Last_Updated)
+#   -   get_all_account_status(self)
+#   -   get_account_status_from_Account_ID(self, Account_ID)
+#   -   get_password(self, login)
+#   -   add_user(self, login, password)
+#   -   get_users(self)
+
+
+
 import mysql.connector
 import yaml
 
@@ -14,6 +39,20 @@ class DBHandler:
         )
 
 
+    # Customer status and account status management :
+    
+    # CustomerStatus Table :
+    # +--------------+-------------+------+-----+---------+-------+
+    # | Field        | Type        | Null | Key | Default | Extra |
+    # +--------------+-------------+------+-----+---------+-------+
+    # | SSN_ID       | int         | NO   | PRI | NULL    |       |
+    # | Customer_ID  | int         | NO   |     | NULL    |       |
+    # | Status       | varchar(20) | NO   |     | NULL    |       |
+    # | Message      | varchar(20) | NO   |     | NULL    |       |
+    # | Last_Updated | varchar(20) | NO   |     | NULL    |       |
+    # +--------------+-------------+------+-----+---------+-------+
+
+
     def add_customer_status(self, SSN_ID, Customer_ID, Status, Message, Last_Updated):
         mycursor = self.db.cursor()
 
@@ -25,19 +64,7 @@ class DBHandler:
 
         print(mycursor.rowcount, "record inserted.")
 
-
-    def add_account_status(self, Customer_ID, Account_ID, Account_Type, Status, Message, Last_Updated):
-        mycursor = self.db.cursor()
-
-        sql = "INSERT INTO AccountStatus(Customer_ID, Account_ID, Account_Type, Status, Message, Last_Updated) VALUES (%s, %s, %s, %s, %s, %s)"
-        val = (Customer_ID, Account_ID, Account_Type, Status, Message, Last_Updated)
-        mycursor.execute(sql, val)
-
-        self.db.commit()
-
-        print(mycursor.rowcount, "record inserted.")
-
-
+    
     def get_customer_status_from_Customer_ID(self, Customer_ID):
         mycursor = self.db.cursor(self)
 
@@ -56,6 +83,31 @@ class DBHandler:
         return result
 
 
+    # AccountStatus Table:
+    # +--------------+-------------+------+-----+---------+-------+
+    # | Field        | Type        | Null | Key | Default | Extra |
+    # +--------------+-------------+------+-----+---------+-------+
+    # | Customer_ID  | int         | NO   |     | NULL    |       |
+    # | Account_ID   | int         | NO   |     | NULL    |       |
+    # | Account_Type | varchar(20) | NO   |     | NULL    |       |
+    # | Status       | varchar(20) | NO   |     | NULL    |       |
+    # | Message      | varchar(20) | NO   |     | NULL    |       |
+    # | Last_Updated | varchar(20) | NO   |     | NULL    |       |
+    # +--------------+-------------+------+-----+---------+-------+
+
+
+    def add_account_status(self, Customer_ID, Account_ID, Account_Type, Status, Message, Last_Updated):
+        mycursor = self.db.cursor()
+
+        sql = "INSERT INTO AccountStatus(Customer_ID, Account_ID, Account_Type, Status, Message, Last_Updated) VALUES (%s, %s, %s, %s, %s, %s)"
+        val = (Customer_ID, Account_ID, Account_Type, Status, Message, Last_Updated)
+        mycursor.execute(sql, val)
+
+        self.db.commit()
+
+        print(mycursor.rowcount, "record inserted.")
+
+
     def get_all_account_status(self):
         mycursor = self.db.cursor(self)
 
@@ -64,14 +116,24 @@ class DBHandler:
         
         return result
 
-
-    def get_customer_status_from_Account_ID(self, Account_ID):
+    def get_account_status_from_Account_ID(self, Account_ID):
         mycursor = self.db.cursor(self)
 
         mycursor.execute("SELECT * FROM AccountStatus where Account_ID={}".format(Account_ID))
         result = mycursor.fetchall()
         
         return result
+
+
+    # UserStore Table :
+    # +----------+-------------+------+-----+-------------------+-------------------+
+    # | Field    | Type        | Null | Key | Default           | Extra             |
+    # +----------+-------------+------+-----+-------------------+-------------------+
+    # | login    | varchar(50) | NO   | PRI | NULL              |                   |
+    # | password | varchar(20) | NO   |     | NULL              |                   |
+    # | time     | timestamp   | YES  |     | CURRENT_TIMESTAMP | DEFAULT_GENERATED |
+    # +----------+-------------+------+-----+-------------------+-------------------+
+
 
     def get_password(self, login):
         mycursor = self.db.cursor(self)
@@ -89,6 +151,7 @@ class DBHandler:
         mycursor.execute(sql, val)
         self.db.commit()
 
+
     def get_users(self):
         mycursor = self.db.cursor(self)
 
@@ -97,10 +160,3 @@ class DBHandler:
         
         return result
 
-    
-
-
-dbh = DBHandler()
-dbh.add_user("User01","qwerty")
-print(dbh.get_password("Rohan"))
-[print(x) for x in dbh.get_users()]
--- database/table.py
+++ database/table.py
@@ -4,15 +4,9 @@
     USE xplore;
 
     CREATE TABLE userstore(
-<<<<<<< HEAD
-        Login VARCHAR(20) NOT NULL PRIMARY KEY,
-        Password VARCHAR(20) NOT NULL,
-        TimeStamp TIMESTAMP NOT NULL
-=======
         login varchar(50) NOT NULL PRIMARY KEY,
         password VARCHAR(20) NOT NULL,
         time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
->>>>>>> 3d1682aa68936cf10aac44d12602286d5d98e710
     );
 
     CREATE TABLE Customer(
@@ -24,13 +18,8 @@
     );
 
     CREATE TABLE Account(
-<<<<<<< HEAD
-        Customer_ID INT NOT NULL PRIMARY KEY,
-        Account_ID INT NOT NULL,
-=======
         Customer_ID INT NOT NULL,
         Account_ID INT NOT NULL PRIMARY KEY,
->>>>>>> 3d1682aa68936cf10aac44d12602286d5d98e710
         Balance INT NOT NULL,
         CR_Data DATE NOT NULL,
         CR_LastDate DATE NOT NULL,
