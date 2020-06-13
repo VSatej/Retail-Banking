012a6d0c8c717e31f8d829033bafab854518cf06
Satej Satyan <v.s.satej1998@gmail.com>
2020-06-13 18:07:31 +0000

    ui,pytest updated

--- test/test_register.py
+++ test/test_register.py
@@ -1,8 +1,8 @@
+import unittest
 from app import app
 import pytest
 
 def test_register():
     response = app.test_client.get('/register')
-
     assert response.status_code == 200
-    assert response.data == ''
+
